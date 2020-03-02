2c32bcd9765996ca7c88d63025ef38c7fa43de0b
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-03-02 13:16:30 -0800

    Wrote basic html to start testing .js methods

---
+++ FrontEnd/Movies.js
@@ -0,0 +1,13 @@
+//Want to have all movies in the db on the screen as default
+//Want to have the option to update each movie's details
+//Want to have the option to add a new movie
+
+//Bonus: want to add/edit/see image for a movie
+//Bonus: want to be able to search by one of the movie's fields, Title, Genre, Director
+
+function printAllMovies(){
+    for(movie in Movies)
+}
+
+
+$("MovieInfo").printAllMovies();

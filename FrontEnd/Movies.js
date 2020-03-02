bf3804d1627d3b857f8b3237007cf176abac0594
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-03-02 15:00:39 -0800

    Added several functions in .js file to print movies to table on main screen

--- FrontEnd/Movies.js
+++ FrontEnd/Movies.js
@@ -5,9 +5,24 @@
 //Bonus: want to add/edit/see image for a movie
 //Bonus: want to be able to search by one of the movie's fields, Title, Genre, Director
 
+$.getJSON("https://localhost:44325/api/movie", function( data ){
+        var items = [];
+        $.each( data, function(key, val) {
+            items.push(val);
+        });
+    })
+//Will run as soon as the script is run
+
 function printAllMovies(){
-    for(movie in Movies)
+    var movieInjection;
+    for(movie in items){
+        movieInjection.append("<tr>" +
+            "<td>" + items[movie].Title + "</td>" +
+            "<td>" + items[movie].Genre + "</td>" +
+            "<td>" + items[movie].Director + "</td>" +
+        "</tr>")
+    }
+    return movieInjection;
 }
 
-
-$("MovieInfo").printAllMovies();
+$("MovieInfo").html(printAllMovies());

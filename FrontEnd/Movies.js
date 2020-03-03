4711bc561d9854868974f630c142785f859bc089
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-03-03 06:33:46 -0800

    Wrote function to print each item in the database's title, genre, and director. Not using jquery, but it works for now

--- FrontEnd/Movies.js
+++ FrontEnd/Movies.js
@@ -5,24 +5,39 @@
 //Bonus: want to add/edit/see image for a movie
 //Bonus: want to be able to search by one of the movie's fields, Title, Genre, Director
 
-$.getJSON("https://localhost:44325/api/movie", function( data ){
-        var items = [];
-        $.each( data, function(key, val) {
-            items.push(val);
-        });
-    })
-//Will run as soon as the script is run
+// $.getJSON("https://localhost:44325/api/movie", function( data ){
+//         var movies = [];
+//         $.each( data, function(key, val) {
+//             movies.push(val);
+//         });
+//     })
+var movies = [];
 
-function printAllMovies(){
-    var movieInjection;
-    for(movie in items){
-        movieInjection.append("<tr>" +
-            "<td>" + items[movie].Title + "</td>" +
-            "<td>" + items[movie].Genre + "</td>" +
-            "<td>" + items[movie].Director + "</td>" +
-        "</tr>")
-    }
-    return movieInjection;
-}
+$.ajax({
+    dataType: 'json',
+    url: "https://localhost:44325/api/movie",
+    type: "get",
+    success: function( data ){
+                $.each( data, function(key, movie) {
+                    document.getElementById("MovieInfo").innerHTML += "<tr>" +
+                    "<td>" + movie["title"] + "</td>" +
+                    "<td>" + movie["genre"] + "</td>" +
+                    "<td>" + movie["director"] + "</td>" +
+                "</tr>";
+                });
+            }
+})
 
-$("MovieInfo").html(printAllMovies());
+// $("MovieInfo").html(printAllMovies());
+
+// function printAllMovies(){
+//     var movieInjection;
+//     for(movie in movies){
+//         movieInjection.appendTo("<tr>" +
+//             "<td>" + movies[movie].Title + "</td>" +
+//             "<td>" + movies[movie].Genre + "</td>" +
+//             "<td>" + movies[movie].Director + "</td>" +
+//         "</tr>")
+//     }
+//     return movieInjection;
+// }

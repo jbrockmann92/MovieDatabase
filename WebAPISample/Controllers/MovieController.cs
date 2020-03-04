688aaeda50db88031f60feb38c9fcf2d0de9ae46
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-03-04 12:11:19 -0800

    Changes to adding picture

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -58,7 +58,10 @@ namespace WebAPISample.Controllers
             movieToChange.Director = movie.Director;
             movieToChange.Genre = movie.Genre;
             movieToChange.Title = movie.Title;
-            movieToChange.ImageUrl = movie.ImageUrl;
+            if (movie.ImageUrl != null)
+            {
+                movieToChange.ImageUrl = movie.ImageUrl;
+            }
 
             _context.Update(movieToChange);
             _context.SaveChanges();
